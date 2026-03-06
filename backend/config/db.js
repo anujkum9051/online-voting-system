4793e46e7261c2f3d4a053eb1bf5bc3b2378d079
anujkum9051 <anujkum9051@gmail.com>
2026-03-06 17:28:11 +0530

    fix mongodb connection for render

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/onlineVotingDB");
+        await mongoose.connect(process.env.MONGO_URI);
 
         console.log("MongoDB Connected Successfully");
     } catch (error) {
-        console.error("MongoDB connection failed");
+        console.error("MongoDB connection failed:", error.message);
         process.exit(1);
     }
 };
